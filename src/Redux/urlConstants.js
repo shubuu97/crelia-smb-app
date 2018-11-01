312030f4e2d9f839f30b0a826cdafe7955dc59d5
jaipur <yogendra.jain@allonblock.com>
2018-11-01 11:08:44 +0530

    added production url

--- src/Redux/urlConstants.js
+++ src/Redux/urlConstants.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV !== 'production')
 }
 else
 {
-APPLICATION_BFF_URL = "http://13.233.72.1:4005";
+APPLICATION_BFF_URL = "https://creliasmb.allonblock.com";
 }
 
 export {APPLICATION_BFF_URL};
