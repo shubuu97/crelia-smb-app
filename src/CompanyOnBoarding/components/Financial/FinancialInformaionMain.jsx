2c47eb9e5686b7c8f6bf22ad6c80191ed047b54c
jaipur <yogendra.jain@allonblock.com>
2018-11-14 14:17:05 +0530

    introduced loanProvider in financial info

--- src/CompanyOnBoarding/components/Financial/FinancialInformaionMain.jsx
+++ src/CompanyOnBoarding/components/Financial/FinancialInformaionMain.jsx
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 /* Other Components*/
 import FinancialInfoUpload from './FinancialInfoUpload'
-import FinancialInformationForm from './FinancialInformationForm'
+import FinancialInformationForm from './FinancialInformationForm';
+import LoanProvider from './LoanProvider';
 
 
 class FinancialInformationMain extends React.Component {
@@ -32,14 +33,23 @@ class FinancialInformationMain extends React.Component {
           <p> Please share some additional information on your company performance.</p>
           <div className="row justify-content-between pt-20">
             <div className="col-sm-12 finan-tab">
+              <div className={this.state.type == 'file' ||this.state.type == 'upload' ? "active" : ""} onClick={() => this.handleSwitch('file')}> Finanacial Data</div> 
+              <div className={this.state.type == 'loanProvider' ? "active" : ""} onClick={() => this.handleSwitch('loanProvider')}>Loan Provider</div>
+            </div>
+          </div>
+
+         {this.state.type=='file'||this.state.type=='upload'? <div className="row justify-content-between pt-20">
+            <div className="col-sm-12 finan-sub-tab">
               <div className={this.state.type == 'file' ? "active" : ""} onClick={() => this.handleSwitch('file')}> Upload File</div> <span>OR</span>
               <div className={this.state.type == 'upload' ? "active" : ""} onClick={() => this.handleSwitch('upload')}>Fill out manually</div>
             </div>
-          </div>
+          </div>:null}
           <div className="row justify-content-between pt-20">
             <div className="col-sm-12">
               {
-                this.state.type == 'file' ? <FinancialInfoUpload isFetchingApprove={this.props.isFetchingApprove} isFetchingSave={this.props.isFetchingSave} handleSubmitAprroval={this.handleSubmitAprroval} handleNext={this.props.handleNext} /> : <FinancialInformationForm isFetchingApprove={this.props.isFetchingApprove} isFetchingSave={this.props.isFetchingSave} initialValues={this.props.initialValues} handleSubmitAprroval={this.handleSubmitAprroval} handleNext={this.props.handleNext} />
+                this.state.type == 'file' ? <FinancialInfoUpload isFetchingApprove={this.props.isFetchingApprove} isFetchingSave={this.props.isFetchingSave} handleSubmitAprroval={this.handleSubmitAprroval} handleNext={this.props.handleNext} /> : 
+                
+            <div>{this.state.type=='upload'?  <FinancialInformationForm isFetchingApprove={this.props.isFetchingApprove} isFetchingSave={this.props.isFetchingSave} initialValues={this.props.initialValues} handleSubmitAprroval={this.handleSubmitAprroval} handleNext={this.props.handleNext} />:<LoanProvider/>}</div>
               }
             </div>
           </div>
