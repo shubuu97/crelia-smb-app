966570220b0292fffb6fc564ed27c33fc172c5c3
Mayuk <mayuk.agarwal@allonblock.com>
2018-11-17 17:02:07 +0530

    dropzone legal buttons

--- src/CompanyProfile/components/legal.js
+++ src/CompanyProfile/components/legal.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import _get from 'lodash/get';
+/* Global Imports*/
 import sidebar from './SideBar.js';
 import DropzoneButton from '../../Global/dropzoneButton'
 import decorateWithOnDrop from '../../Global/onDropDecorater';
-import { withState, compose } from 'recompose';
-
-import { Field, reduxForm } from 'redux-form';
-import RFTextField from '../../Global/GlobalTextField';
-import FormControl from '@material-ui/core/FormControl';
-import companypresentation from '../../Assets/images/company-presentation.png';
-import { connect } from 'react-redux';
 import LoaderButton from '../../Global/LoaderButton';
-import _get from 'lodash/get';
+/* Recompose*/
+import { withState, compose } from 'recompose';
+/* Redux Imports*/
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 import { APPLICATION_BFF_URL } from '../../Redux/urlConstants';
 import { postData } from '../../Redux/postAction';
 import { getData } from '../../Redux/getAction';
 import showMessage from '../../Redux/toastAction';
+/* Material Imports */
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 var jwtDecode = require('jwt-decode');
 
 class Legal extends Component {
@@ -74,16 +75,34 @@ class Legal extends Component {
             })
     }
 
+    dropzoneImageProgressPopulate = (name) => {
+        return (
+            _get(this.props, `state.${name}uploadProgress`) ?
+                <div className="flex-column align-center justify-center mt-16">
+                    <CircularProgress
+                        className="progress"
+                        variant="static"
+                        value={_get(this.props, `state.${name}uploadProgress`)}
+                    />
+                    <span className="progress-text">
+                        {_get(this.props, `state.${name}uploadProgress`)} %
+                    </span>
+                </div>
+                :
+                <img src={_get(this.props, `state.${name}link`) || _get(this.props, { name } + 'Link')} className="legal-images" />
+        )
+    }
+
     render() {
         let { handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit(this.handleSubmitLegal)}>
                 <div>
                     <div className="row">
-                        <div className="col-sm-4">
+                        <div className="col-sm-4 flex-column align-center">
                             <h4>Registration Certificate</h4>
                             <div className="legal-images-container">
-                                <img src={_get(this.props, 'state.registrationCertificatelink') || _get(this.props, 'registrationCertificateLink')} className="legal-images"/>
+                                {this.dropzoneImageProgressPopulate('registrationCertificate')}
                             </div>
                             <br />
                             <DropzoneButton
@@ -91,10 +110,10 @@ class Legal extends Component {
                                 name="Upload File"
                                 onDrop={this.props.onDrop} />
                         </div>
-                        <div className="col-sm-4">
+                        <div className="col-sm-4 flex-column align-center ">
                             <h4>Organizational Chart</h4>
                             <div className="legal-images-container">
-                                <img src={_get(this.props, 'state.organizationalChartlink') || _get(this.props, 'organizationalChartLink')} className="legal-images"/>
+                                {this.dropzoneImageProgressPopulate('organizationalChart')}
                             </div>
                             <br />
                             <DropzoneButton
@@ -102,16 +121,16 @@ class Legal extends Component {
                                 name="Upload File"
                                 onDrop={this.props.onDrop} />
                         </div>
-                        <div className="col-sm-4">
+                        <div className="col-sm-4 flex-column align-center ">
                             <h4>Tax Certificate</h4>
                             <div className="legal-images-container">
-                                <img src={_get(this.props, 'state.taxCertificatelink') || _get(this.props, 'taxCertificateLink')} className="legal-images"/>
+                                {this.dropzoneImageProgressPopulate('taxCertificate')}
                             </div>
-                            
+
                             <br />
                             <DropzoneButton
                                 fieldName='taxCertificate'
-                                name="Upload File" 
+                                name="Upload File"
                                 onDrop={this.props.onDrop} />
                         </div>
                         <div class="action-block">
--- src/Global/dropzoneButton.js
+++ src/Global/dropzoneButton.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 
-let dropzoneButton = (props)=>
-{
+let dropzoneButton = (props) => {
   const dropzoneRef = React.createRef();
 
-return(
-  <div>
-  <Dropzone style={{}}
-   ref={dropzoneRef} 
-   onDrop={(accept,reject)=>props.onDrop(accept,reject,props.fieldName)}>
-  </Dropzone>
-  <button type="button" className="btnsecondary" onClick={() => { dropzoneRef.current.open() }}>
-    {props.name}
-  </button>
-  </div>)
+  return (
+    <div>
+      
+      <Dropzone
+        style={{}}
+        ref={dropzoneRef}
+        onDrop={(accept, reject) => props.onDrop(accept, reject, props.fieldName)}>
+      </Dropzone>
+
+      <button type="button" className="btnsecondary" onClick={() => { dropzoneRef.current.open() }}>
+        {props.name}
+      </button>
+    </div>)
 }
 export default dropzoneButton;
