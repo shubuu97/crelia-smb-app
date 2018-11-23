b89d8e4e4460944131bf506cc819becacffb27ce
jaipur <yogendra.jain@allonblock.com>
2018-11-23 11:35:48 +0530

    parsed service suport aded in network list

---
+++ src/CompanyOnBoarding/components/Financial/parsedPreview.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+export default class ParsedPreview extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                Parsed content will come here
+            </div>
+        )
+    }
+}
--- src/Global/onDropDecorater.js
+++ src/Global/onDropDecorater.js
@@ -1,39 +1,47 @@
-import {withHandlers} from 'recompose';
+import { withHandlers } from 'recompose';
 import { connect } from 'tls';
-import {postData} from '../Redux/postAction';
-import {getData} from '../Redux/getAction'
+import { postData } from '../Redux/postAction';
+import { getData } from '../Redux/getAction'
 import showMessage from '../Redux/toastAction';
-import {APPLICATION_BFF_URL} from '../Redux/urlConstants'
+import { APPLICATION_BFF_URL } from '../Redux/urlConstants'
 const decorateWithOnDrop = withHandlers({
-    onDrop:(props)=>(accept,reject,name)=>{
-        console.log(accept,"accept is here");
+    onDrop: (props) => (accept, reject, name) => {
+        console.log(accept, "accept is here");
         let formData = new FormData();
         formData.append('file', accept[0])
         formData.append('mediaType', 'customer')
         formData.append('mediaTypeId', '1234567');
         var percentCompleted = 0;
-          let uploadConfig =  function(progressEvent) {
-            let  percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
-           
-           console.log(percentCompleted,"percent complete is here")
-             props.setState({...props.state,[name+'uploadProgress']:percentCompleted})
-            }
-          
-        props.dispatch(postData(`${APPLICATION_BFF_URL}/api/media`, formData, 'fileUpload',{
-            init:`File_Upload_init`,
-            success:'File_Upload_Success',
-            error:'File_Upload_Error'
+        let uploadConfig = function (progressEvent) {
+            let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
 
-        },'post',uploadConfig))
+            console.log(percentCompleted, "percent complete is here")
+            props.setState({ ...props.state, [name + 'uploadProgress']: percentCompleted })
+        }
+
+        props.dispatch(postData(`${APPLICATION_BFF_URL}/api/media`, formData, 'fileUpload', {
+            init: `File_Upload_init`,
+            success: 'File_Upload_Success',
+            error: 'File_Upload_Error'
+
+        }, 'post', uploadConfig))
             .then((data) => {
-                let linkname = name+'link'
-                props.setState({...props.state,[name+'link']:data.message.absoluteURL})
-                // props.dispatch(getData(`${APPLICATION_BFF_URL}/parser-service/cashFlowParser?files=${data.message.absoluteURL}`,'fileUpload',{
-                //     init:'File_Upload_init',
-                //     success:'File_Upload_Success',
-                //     error:'File_Upload_Error'
-        
-                // }))
+                props.setState({ ...props.state, [name + 'link']: data.message.absoluteURL })
+                if (props.parsedData) {
+                    let linkname = name + 'link'
+                   
+                    props.dispatch(getData(`${APPLICATION_BFF_URL}/parser-service/cashFlowParser?files=${data.message.absoluteURL},`, 'fileUpload', {
+                        init: 'Parse_Data_init',
+                        success: 'Parse_Data_Success',
+                        error: 'Parse_Data_Error'
+
+                    }).then((data) => {
+                      if(props.afterParseFunction)
+                      {
+                          props.afterParseFunction(data);
+                      }
+                    }))
+                }
                 props.dispatch(showMessage({ text: 'Upload success', isSuccess: true }));
                 setTimeout(() => {
                     props.dispatch(showMessage({ text: '', isSuccess: true }));
@@ -41,14 +49,14 @@ const decorateWithOnDrop = withHandlers({
                 }, 1000)
             })
             .catch((error) => {
-                console.log(error,"error")
+                console.log(error, "error")
                 props.dispatch(showMessage({ text: 'Some error occured', isSuccess: false }));
                 setTimeout(() => {
-                 props.dispatch(showMessage({ text: '', isSuccess: true }));
+                    props.dispatch(showMessage({ text: '', isSuccess: true }));
 
                 }, 1000)
             })
-        props.setState({...props.state,[name]:accept[0]})
+        props.setState({ ...props.state, [name]: accept[0] })
     }
 })
 
