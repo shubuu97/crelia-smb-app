618b16b08e532a51bfb209e3d2b13dcc76f7d8e0
jaipur <yogendra.jain@allonblock.com>
2018-12-19 15:10:59 +0530

    populate data for equity

--- src/SMB/HistoryPages/LoanHistory/component/PopulateDataDetail.jsx
+++ src/SMB/HistoryPages/LoanHistory/component/PopulateDataDetail.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import _get from 'lodash/get';
 import moment from 'moment';
+
 const PopulateDataDetails = (props) => {
-    let allData = _get(props, 'allData.fund', [])
-    let parseData = {
+    let allData = _get(props, 'allData.fund', []);
+    let parseData = {}
+      // For Equity Request
+      if(_get(allData, '$class') === 'com.aob.crelia.fund.Equity')
+      parseData = {
+          Money: `${_get(allData, 'money.amount', '-')}`,
+          Currency: `${_get(allData, 'money.currency', '-')}`,
+          //Term: `${_get(allData, 'term', '-')} yrs`,
+          TimeFrame: `${moment(_get(allData, 'timeFrame', '-')).format('DD-MM-YYYY')}`,
+          FundAllocation: _get(allData, 'fundAllocation', '-'),
+          Range: `${_get(allData, 'lowerValue', '0')} - ${_get(allData, 'upperValue', '0')}${' '}(%)`,
+          SAFE: `${_get(allData, 'isSafeOffering', '-')}`,
+          SafeDiscount: `${_get(allData, 'safeDiscount', '-')}`,
+          SafeMarketCap: `${_get(allData, 'safeMarketCap', '-')}`,
+          NationAgreement: `${_get(allData, 'isNationAgreement', '-')}`,
+          BoardMembership: `${_get(allData, 'isBoardMembership', '-')}`,
+          Comments: _get(allData, 'comment', '-'),
+      }
+      else
+      {
+       parseData = {
         MoneyRange: `${_get(allData, 'moneyRange.minAmount')} - ${_get(allData, 'moneyRange.maxAmount')}`,
         Currency: `${_get(allData, 'moneyRange.currency')}`,
         Term: `${_get(allData, 'term')} yrs`,
@@ -12,6 +32,7 @@ const PopulateDataDetails = (props) => {
         InterestRateType: `${_get(allData, 'interestRateType')}`,
         DesiredRate: `${_get(allData, 'interestRate')} %`,
     }
+}
     let data = []
     Object.keys(parseData).map((key) => {
         let title = key.replace(/([A-Z])/g, ' $1')
