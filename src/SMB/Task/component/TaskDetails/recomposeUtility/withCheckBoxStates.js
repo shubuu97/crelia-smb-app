3e69c0f44bed37e2e81a23e8a6e9a15f7493e1a4
jaipur <yogendra.jain@allonblock.com>
2018-12-29 20:25:17 +0530

    added mapping in tasks

--- src/SMB/Task/component/TaskDetails/recomposeUtility/withCheckBoxStates.js
+++ src/SMB/Task/component/TaskDetails/recomposeUtility/withCheckBoxStates.js
@@ -1,13 +1,69 @@
 import { withHandlers } from "recompose";
 
+
+const modifyName = (name) => {
+    switch (name) {
+        case 'CompanyName':
+            return 'LEGALNAME'
+            break;
+        case 'BalanceSheet':
+            return 'FINANCIALINFO_BALANCESHEET'
+            break;
+        case 'IncomeStatement':
+            return 'FINANCIALINFO_INCOMESTATEMENT'
+            break;
+        case 'Forecast':
+            return 'FINANCIALINFO_FORECAST'
+            break;
+        case 'BusinessPlan':
+            return 'FINANCIALINFO_BUSINESSPLAN'
+            break;
+        case 'CashFlow':
+            return 'FINANCIALINFO_CASHFLOW'
+            break;
+        case 'OrganizationLink':
+            return 'URL'
+            break;
+        case 'Street-1':
+            return 'ADDRESS_LINE1'
+            break;
+        case 'Street-2':
+            return 'ADDRESS_LINE2'
+            break;
+        case 'City':
+            return 'ADDRESS_CITY'
+            break;
+        case 'ZipCode':
+            return 'ADDRESS_ZIPCODE'
+            break;
+        case 'Country':
+            return 'ADDRESS_COUNTRY'
+            break;
+        case 'Region':
+            return 'ADDRESS_REGION'
+            break;
+        case 'PhoneNumber':
+            return 'PHONENUMBER'
+            break;
+        case 'Email':
+            return 'EMAIL'
+            break;
+        default:
+            return name
+            break;
+    }
+}
+
 const handleChange = props => name => event => {
     debugger;
     let allFields = { ...props.fields };
-    allFields[name] = event.target.checked
+    //Logic to change the names according to sending to ui
+    name = modifyName(name)
+    allFields[name] = event.target.checked;
     props.FieldAccessReqTask(allFields);
-    return {[name]: event.target.checked };
+    return { [name]: event.target.checked };
 };
 
-const withCheckBoxStates = withHandlers({handleChange});
+const withCheckBoxStates = withHandlers({ handleChange });
 
 export default withCheckBoxStates;
