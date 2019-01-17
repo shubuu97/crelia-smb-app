6d14c557cbbaecea12c45be511724e587be5cc41
Mayuk <mayuk.agarwal@allonblock.com>
2019-01-17 18:19:26 +0530

    Card Notification view

--- src/xBoilerplate/CardNotificationTest.jsx
+++ src/xBoilerplate/CardNotificationTest.jsx
@@ -8,8 +8,40 @@ import _get from 'lodash/get';
 /* Component Imports */
 import CardNotification from '../Global/Components/CardNotification/CardNotification'
 
-// Todo : Dummy Data Structure
-// Todo : 
+// // Todo : Dummy Data Structure
+// Todo : Get non copyright images for random pool
+
+let dummyData = [
+    {
+        NotificationType : 'Investor Requesting data',
+        Time: 'now',
+        Title: 'Some Title 1',
+        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        AllowedActions: [],
+    },
+    {
+        NotificationType : 'Investor Offer',
+        Time: 'yesterday',
+        Title: 'Some Title 2',
+        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        Image:'',
+        AllowedActions: [],
+    },
+    {
+        NotificationType : 'Loan Approved',
+        Time: '12/01/19',
+        Title: 'Some Title 3',
+        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        AllowedActions: [],
+    },
+    {
+        NotificationType : 'Account Created',
+        Time: '20/12/18',
+        Title: 'Some Title 4',
+        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        AllowedActions: [],
+    },
+]
 
 class CardNotificationTest extends React.Component {
 
@@ -25,6 +57,7 @@ class CardNotificationTest extends React.Component {
             <div>
                 <CardNotification
                     header='Notifications'
+                    data = {dummyData}
                 />
             </div>
         );
